d82e26fe51fbb4358c130fc8e883afc7f278b1ef
Amarjeet Maurya <30472038+amar969@users.noreply.github.com>
2022-09-20 05:07:05 +0530

    reverse Triangle added

--- PatternPrinting/Triangle.js
+++ PatternPrinting/Triangle.js
@@ -1,11 +1,21 @@
 const solve = (N) => {
+    let st = 1 
+    let sp = N - 1
     for(let i=0; i<N; i++){
         let final = ""
-        for(let j=0; j<=i; j++){
+        for(let j=1; j<=sp; j++){
+            final += " "
+        }
+
+        for(let j=0; j<st; j++){
             final += "*"
         }
+
+        st+=2 
+        sp--
         console.log(final)
     }
+
 }
 
 solve(5)
--- PatternPrinting/reverseTriangle.js
+++ PatternPrinting/reverseTriangle.js
@@ -1,3 +1,19 @@
 const solve = (N) => {
-    
+    let sp = 0
+    let st = 2*N
+    for(let i=0; i<N; i++){
+        let final = ""
+        for(let j=0; j<sp; j++){
+            final += " "
+        }
+
+        for(let j=1; j<st; j++){
+            final += "*"
+        }
+        sp++
+        st-=2
+        console.log(final)
+    }
 }
+
+solve(5)
