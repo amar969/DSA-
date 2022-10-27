55bfd4a4d32eff45e487d7b702bfc8ae08b9f0ed
Amarjeet Maurya <30472038+amar969@users.noreply.github.com>
2022-10-27 06:57:49 +0530

    Intersection code optimized

--- Array/IntersectionArr.js
+++ Array/IntersectionArr.js
@@ -14,12 +14,12 @@ solve(3, [4, 5, 7], [9, 2, 5])
 
 
 const solve1 = (N, arr1, arr2) => {
-    let ans = 0
+    let ans = []
     let l = 0 
     let r = N-1 
     while(l < N && r < N){
         if(arr1[l] == arr2[r]){
-            ans = arr1[l]
+            ans.push(arr1[l])
             return
         }
         else if (arr1[l] > arr2[r]){
@@ -30,5 +30,21 @@ const solve1 = (N, arr1, arr2) => {
     console.log(ans)
 }
 
-solve(3, [4, 5, 7], [9, 2, 5])
+solve1(3, [4, 5, 7], [9, 2, 5])
+
+const solve2 = (nums1, nums2) => {
+    let ans = []
+    for(let i=0; i<nums2.length; i++){
+        let val = nums2[i]
+        if(nums1.includes(val)) {
+            let spval = nums1.splice(nums1.indexOf(val), 1)
+            ans.push(val)
+        }
+    }
+    
+    return ans
+}
+
+console.log(solve2([1,2,2,1], [2,2]))
+console.log(solve2([4,9,5], [9,4,9,8,4]))
 
--- Array/plusOne.js
+++ Array/plusOne.js
@@ -1,18 +1,7 @@
 const solve = (digits) => {
-    let final = []
-    for(let i=0; i<digits.length; i++){
-        
-        if(i == digits.length - 1){
-            if(digits[i] == 9) {
-                final.push(1)
-                final.push(0)
-            }
-            else final.push(digits[i] + 1)
-        }
-        else final.push(digits[i])
-        
-    }
-    return final
+    let res = digits.join("")
+    res = Number(res) + 1
+    return String(res).split("")
 }
 
 console.log(solve([1,2,3]))
