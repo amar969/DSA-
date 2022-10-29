969c78f33ac1950b6ffb7148014ae50a10714bbd
Amarjeet Maurya <30472038+amar969@users.noreply.github.com>
2022-10-29 11:21:21 +0530

    Two sum one more method added

--- Array/TwoSum.js
+++ Array/TwoSum.js
@@ -19,3 +19,15 @@ const solve = (n,k, arr) => {
 }
 
 console.log(solve(4,9,[2,7,11,15]))
+
+// two sum is array is not sorted
+
+const solve1 = (nums, target) => {
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] == target) return [i, j]
+        }
+    }
+}
+
+console.log(solve1([2,7,11,15], 9))
